672f0723c2ac606c231e370c282fef5595df8e6e
thinkofher <piaskupiasku@gmail.com>
2019-07-18 14:43:39 +0200

    Implemented data structure and simple methods for single task

---
+++ db/task.go
@@ -0,0 +1,27 @@
+package db
+
+import (
+	"encoding/json"
+	"time"
+)
+
+// Represents single taks
+type Task struct {
+	Id   int       `json:"id"`
+	Body string    `json:"body"`
+	Tags []string  `json:"tags"`
+	Date time.Time `json:"date"`
+}
+
+// Returns the JSON encoding of task struct
+func (t Task) ToJson() ([]byte, error) {
+	return json.Marshal(t)
+}
+
+func TaskFromJson(jsonBytes []byte, t *Task) error {
+	return json.Unmarshal(jsonBytes, &t)
+}
+
+func TasksFromJson(jsonBytes []byte, t *[]Task) error {
+	return json.Unmarshal(jsonBytes, &t)
+}

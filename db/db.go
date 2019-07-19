6aee56c0408ff0576f2f1cfb02cdfb33f38181eb
thinkofher <piaskupiasku@gmail.com>
2019-07-19 10:43:58 +0200

    Added functions for adding and getting tasks to and from database

--- db/db.go
+++ db/db.go
@@ -1,26 +1,28 @@
 // Package db provides functions and data structures
 // for manipulating local go boltdb database in terms of
-// creating and editing tasks
+// creating and editing tasks.
 package db
 
 import (
+	"encoding/binary"
 	"fmt"
 	"time"
 
 	"github.com/boltdb/bolt"
 )
 
+var db *bolt.DB
 var taskBucket = []byte("tasks")
 
-// InitDB initialize default buckets in given path
+// InitDB initialize default buckets in given path.
 func InitDB(dbPath string) error {
-	db, err := bolt.Open(
-		dbPath, 0600, &bolt.Options{Timeout: 1 * time.Second})
+	var err error
 
+	db, err = bolt.Open(
+		dbPath, 0600, &bolt.Options{Timeout: 1 * time.Second})
 	if err != nil {
 		return err
 	}
-	defer db.Close()
 
 	return db.Update(func(tx *bolt.Tx) error {
 		_, err := tx.CreateBucketIfNotExists(taskBucket)
@@ -31,6 +33,48 @@ func InitDB(dbPath string) error {
 		return nil
 	})
 }
+
+func AddTask(t *Task) error {
+	return db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket(taskBucket)
+
+		id, err := b.NextSequence()
+		if err != nil {
+			return err
+		}
+		t.Id = int(id)
+
+		buf, err := t.ToJson()
+		if err != nil {
+			return err
+		}
+
+		return b.Put(itob(t.Id), buf)
+	})
+}
+
+func GetTask(id int) (Task, error) {
+	var t Task
+	var jsonTask []byte
+
+	err := db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket(taskBucket)
+		jsonTask = b.Get(itob(id))
+		return nil
+	})
+	if err != nil {
+		return t, err
+	}
+
+	err = TaskFromJson(jsonTask, &t)
+	if err != nil {
+		return t, err
+	}
+
+	return t, nil
+
+}
+
 // itob returns an 8-byte big endian representation of v.
 func itob(v int) []byte {
 	b := make([]byte, 8)

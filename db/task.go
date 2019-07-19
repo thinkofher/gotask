bee2ba0b89217f8ca61e62b6696faed6392274ac
thinkofher <piaskupiasku@gmail.com>
2019-07-19 10:40:24 +0200

    Added method for setting tags from a continuos string of characters

--- db/task.go
+++ db/task.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"encoding/json"
+	"strings"
 	"time"
 )
 
@@ -20,6 +21,12 @@ func (t Task) ToJson() ([]byte, error) {
 
 func (t *Task) SetCurrDate() {
 	t.Date = time.Now()
+}
+
+// Creates a list of strings (tags) from tags in
+// string separated with given character
+func (t *Task) ParseTags(tagStr string, sep string) {
+	t.Tags = strings.Split(tagStr, sep)
 }
 
 func TaskFromJson(jsonBytes []byte, t *Task) error {
